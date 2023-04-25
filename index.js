24c092cf1b75a4c6b3b1d27959bc83f417fbf9bb
hilbert-mitra <hilbert.mitra123@gmail.com>
2023-04-25 14:24:45 -0300

    update

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const port = 3002;
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+const fs = require("fs");
 
-app.use(express.static("public"));
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.post("/sendMail", function (_req, resp) {
+  resp.header("Access-Control-Allow-Origin", "*");
+
+  fs.readFile("./invite/index.html", "utf8", function (err, data) {
+    if (err) throw err;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: "mitra.industria.dev@gmail.com",
+        pass: "porqvzmvkqrmkarp",
+      },
+    });
+
+    transporter
+      .sendMail({
+        from: "mitraecp@gmail.com",
+        to: "mitra.industria.dev@gmail.com",
+        subject: `teste sender`,
+        html: data,
+      })
+      .then(() => {
+        resp.end(`Email sended`);
+      })
+      .catch(console.error);
+  });
 });
+
+app.listen(8080);
