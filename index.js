890d7c0c3109705c6b5e83c628d92c393c28befa
hilbs <danillopkt@gmail.com>
2024-10-22 14:43:42 -0300

    update

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.post("/sendMail", function (_req, resp) {
+app.post("/send-invite", function (_req, resp) {
   resp.header("Access-Control-Allow-Origin", "*");
 
   fs.readFile("./invite/index.html", "utf8", function (err, data) {
@@ -39,4 +39,35 @@ app.post("/sendMail", function (_req, resp) {
   });
 });
 
-app.listen(8080);
+app.post("/send-confirm", function (_req, resp) {
+  resp.header("Access-Control-Allow-Origin", "*");
+
+  fs.readFile("./confirmation_code/index.html", "utf8", function (err, data) {
+    if (err) throw err;
+
+    const transporter = nodemailer.createTransport({
+      host: "smtp.gmail.com",
+      port: 587,
+      auth: {
+        user: "mitra.industria.dev@gmail.com",
+        pass: "porqvzmvkqrmkarp",
+      },
+    });
+
+    transporter
+      .sendMail({
+        from: "mitraecp@gmail.com",
+        to: "mitra.industria.dev@gmail.com",
+        subject: `teste sender`,
+        html: data,
+      })
+      .then(() => {
+        resp.end(`Email sended`);
+      })
+      .catch(console.error);
+  });
+});
+
+app.listen(8080, () => {
+  console.log("app ouvindo na porta 8080");
+});
